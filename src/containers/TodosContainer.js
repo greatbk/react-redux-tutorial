b8acf416aa5ddc8e531bc2561cd8a709a83deabc
이병기 <comlbg@ls-electric.com>
2023-07-07 08:27:49 +0900

    .

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,23 +1,30 @@
 import Todos from "../components/Todos";
 import { changeInput, insert, remove, toggle } from "../modules/todos";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { useCallback } from "react";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(
+    (input) => dispatch(changeInput(input)),
+    [dispatch]
+  );
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
